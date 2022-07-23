f64946a118e002d0559fe4e2da97f85e09cd86d0
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2022-07-23 17:36:17 +0200

    Updated build scripts

--- src/build-release.sh
+++ src/build-release.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#: Title       : build-debug.sh
+#: Title       : build-release.sh
 #: Date        : 2022-07-23
 #: Author      : Kjetil Kristoffer Solberg <post@ikjetil.no>
 #: Version     : 1.0
-#: Description : Builds ccat.
+#: Description : Builds its-skip.
 echo "Building its-skip ..."
-echo "> using debug build < "
+echo "> using release build < "
 
-g++ its-skip.cpp -o its-skip -ggdb
+g++ its-skip.cpp -o its-skip
 if [[ $? -eq 0 ]]
 then
     echo "> its-skip build ok"
