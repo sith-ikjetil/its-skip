e6e7d8451ab99c04a06f3fccb3efc9c75c07ae01
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2022-07-23 15:04:37 +0200

    Updated build scripts

--- src/build-debug.sh
+++ src/build-debug.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+echo "Building its-skip ..."
+echo "> Using debug build < "
+
 g++ its-skip.cpp -o its-skip -ggdb
+if [[ $? -eq 0 ]]
+then
+    echo "> its-skip build Ok"
+else
+    echo "> its-skip build Error"
+fi
+
+echo "> Build Process Complete <"
