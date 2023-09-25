4c6761ec81bf4c4d61ed4c83f5d49ce177098916
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2023-09-25 12:36:20 +0200

    Improved man pages install

--- man/install.sh
+++ man/install.sh
@@ -4,12 +4,43 @@
 #: Author      : Kjetil Kristoffer Solberg <post@ikjetil.no>
 #: Version     : 1.0
 #: Description : Installs its-skip man pages.
+
+function InstallUbuntu()
+{
+    if [ ! -d "/usr/local/man/man1" ]
+    then
+        sudo mkdir /usr/local/man/man1
+    fi
+
+    sudo cp its-skip.1 /usr/local/man/man1
+    sudo chmod go+r /usr/local/man/man1/its-skip.1
+    printf "Install Ubuntu Complete\n"
+}
+
+function InstallFedora()
+{
+    # fedora
+    if [ ! -d "/usr/local/share/man/man1" ]
+    then
+        sudo mkdir "/usr/local/share/man/man1"
+    fi        
+    sudo cp its-skip.1 /usr/local/share/man/man1
+    sudo chmod go+r /usr/local/share/man/man1/its-skip.1
+    printf "Install Fedora Complete\n"
+}
+
+
 printf "Installing man page\n"
+
 ./render.sh
-if [ ! -d "/usr/local/man/man1" ]
+
+if [ -d "/usr/local/man" ]
 then
-    sudo mkdir /usr/local/man/man1
+    InstallUbuntu
 fi
-sudo cp its-skip.1 /usr/local/man/man1
-sudo chmod go+r /usr/local/man/man1/its-skip.1
-printf "...install complete\n"
+
+if [ -d "/usr/local/share/man" ]
+then
+    InstallFedora
+fi
+
